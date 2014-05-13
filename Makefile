c0cc1a0cd70f8ff838aee32e5e105d40f1df46ff
Yuki Yugui Sonoda <yugui@google.com>
2014-05-13 18:01:02 +0900

    Initial commit for opensourceing protoc-gen-bq-schema

---
+++ Makefile
@@ -0,0 +1,50 @@
+# Copyright 2014 Google Inc. All rights reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+BQ_PLUGIN=bin/protoc-gen-bq-schema
+GO_PLUGIN=bin/protoc-gen-go
+PROTOC_GEN_GO_PKG=code.google.com/p/goprotobuf/protoc-gen-go
+GLOG_PKG=github.com/golang/glog
+PROTO_PKG=code.google.com/p/goprotobuf/proto
+PKGMAP=Mgoogle/protobuf/descriptor.proto=$(PROTOC_GEN_GO_PKG)/descriptor
+
+install: $(BQ_PLUGIN)
+
+$(BQ_PLUGIN): bq_table_name.pb.go goprotobuf glog
+	go build -o $@
+
+bq_table_name.pb.go: bq_table_name.proto $(GO_PLUGIN)
+	protoc -I. -Ivendor/protobuf --plugin=bin/protoc-gen-go --go_out=$(PKGMAP):. bq_table_name.proto
+
+goprotobuf:
+	go get $(PROTO_PKG)
+
+glog:
+	go get $(GLOG_PKG)
+
+$(GO_PLUGIN):
+	go get $(PROTOC_GEN_GO_PKG)
+	go build -o $@ $(PROTOC_GEN_GO_PKG)
+
+test: $(PROTO_SRC)
+	go test
+
+distclean clean:
+	go clean
+	rm -f $(GO_PLUGIN) $(BQ_PLUGIN)
+
+realclean: distclean
+	rm -f bq_table_name.pb.go
+
+.PHONY: goprotobuf glog
---
+++ main.go
@@ -0,0 +1,364 @@
+// Copyright 2014 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// protoc plugin which converts .proto to schema for BigQuery.
+// It is spawned by protoc and generates schema for BigQuery, encoded in JSON.
+//
+// usage:
+//  $ bin/protoc --bq-schema_out=path/to/outdir foo.proto
+//
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+
+	"code.google.com/p/goprotobuf/proto"
+	descriptor "code.google.com/p/goprotobuf/protoc-gen-go/descriptor"
+	plugin "code.google.com/p/goprotobuf/protoc-gen-go/plugin"
+	"github.com/golang/glog"
+)
+
+var (
+	globalPkg = &ProtoPackage{
+		name:     "",
+		parent:   nil,
+		children: make(map[string]*ProtoPackage),
+		types:    make(map[string]*descriptor.DescriptorProto),
+	}
+)
+
+// Field describes the schema of a field in BigQuery.
+type Field struct {
+	Name   string   `json:"name"`
+	Type   string   `json:"type"`
+	Mode   string   `json:"mode"`
+	Fields []*Field `json:"fields,omitempty"`
+}
+
+// ProtoPackage describes a package of Protobuf, which is an container of message types.
+type ProtoPackage struct {
+	name     string
+	parent   *ProtoPackage
+	children map[string]*ProtoPackage
+	types    map[string]*descriptor.DescriptorProto
+}
+
+func registerType(pkgName *string, msg *descriptor.DescriptorProto) {
+	pkg := globalPkg
+	if pkgName != nil {
+		for _, node := range strings.Split(*pkgName, ".") {
+			if pkg == globalPkg && node == "" {
+				// Skips leading "."
+				continue
+			}
+			child, ok := pkg.children[node]
+			if !ok {
+				child = &ProtoPackage{
+					name:     pkg.name + "." + node,
+					parent:   pkg,
+					children: make(map[string]*ProtoPackage),
+					types:    make(map[string]*descriptor.DescriptorProto),
+				}
+				pkg.children[node] = child
+			}
+			pkg = child
+		}
+	}
+	pkg.types[msg.GetName()] = msg
+}
+
+func (pkg *ProtoPackage) lookupType(name string) (*descriptor.DescriptorProto, bool) {
+	if strings.HasPrefix(name, ".") {
+		return globalPkg.relativelyLookupType(name[1:len(name)])
+	}
+
+	for ; pkg != nil; pkg = pkg.parent {
+		if desc, ok := pkg.relativelyLookupType(name); ok {
+			return desc, ok
+		}
+	}
+	return nil, false
+}
+
+func relativelyLookupNestedType(desc *descriptor.DescriptorProto, name string) (*descriptor.DescriptorProto, bool) {
+	components := strings.Split(name, ".")
+componentLoop:
+	for _, component := range components {
+		for _, nested := range desc.GetNestedType() {
+			if nested.GetName() == component {
+				desc = nested
+				continue componentLoop
+			}
+		}
+		glog.Infof("no such nested message %s in %s", component, desc.GetName())
+		return nil, false
+	}
+	return desc, true
+}
+
+func (pkg *ProtoPackage) relativelyLookupType(name string) (*descriptor.DescriptorProto, bool) {
+	components := strings.SplitN(name, ".", 2)
+	switch len(components) {
+	case 0:
+		glog.V(1).Info("empty message name")
+		return nil, false
+	case 1:
+		found, ok := pkg.types[components[0]]
+		return found, ok
+	case 2:
+		glog.Infof("looking for %s in %s at %s (%v)", components[1], components[0], pkg.name, pkg)
+		if child, ok := pkg.children[components[0]]; ok {
+			found, ok := child.relativelyLookupType(components[1])
+			return found, ok
+		}
+		if msg, ok := pkg.types[components[0]]; ok {
+			found, ok := relativelyLookupNestedType(msg, components[1])
+			return found, ok
+		}
+		glog.V(1).Infof("no such package nor message %s in %s", components[0], pkg.name)
+		return nil, false
+	default:
+		glog.Fatal("not reached")
+		return nil, false
+	}
+}
+
+func (pkg *ProtoPackage) relativelyLookupPackage(name string) (*ProtoPackage, bool) {
+	components := strings.Split(name, ".")
+	for _, c := range components {
+		var ok bool
+		pkg, ok = pkg.children[c]
+		if !ok {
+			return nil, false
+		}
+	}
+	return pkg, true
+}
+
+func convertField(curPkg *ProtoPackage, desc *descriptor.FieldDescriptorProto, msg *descriptor.DescriptorProto) (*Field, error) {
+	field := &Field{
+		Name: desc.GetName(),
+	}
+
+	switch desc.GetType() {
+	case descriptor.FieldDescriptorProto_TYPE_DOUBLE,
+		descriptor.FieldDescriptorProto_TYPE_FLOAT:
+		field.Type = "FLOAT"
+
+	case descriptor.FieldDescriptorProto_TYPE_INT64,
+		descriptor.FieldDescriptorProto_TYPE_UINT64,
+		descriptor.FieldDescriptorProto_TYPE_INT32,
+		descriptor.FieldDescriptorProto_TYPE_UINT32,
+		descriptor.FieldDescriptorProto_TYPE_FIXED64,
+		descriptor.FieldDescriptorProto_TYPE_FIXED32,
+		descriptor.FieldDescriptorProto_TYPE_SFIXED32,
+		descriptor.FieldDescriptorProto_TYPE_SFIXED64,
+		descriptor.FieldDescriptorProto_TYPE_SINT32,
+		descriptor.FieldDescriptorProto_TYPE_SINT64:
+		field.Type = "INTEGER"
+
+	case descriptor.FieldDescriptorProto_TYPE_STRING,
+		descriptor.FieldDescriptorProto_TYPE_BYTES,
+		descriptor.FieldDescriptorProto_TYPE_ENUM:
+		field.Type = "STRING"
+
+	case descriptor.FieldDescriptorProto_TYPE_BOOL:
+		field.Type = "BOOLEAN"
+
+	case descriptor.FieldDescriptorProto_TYPE_GROUP,
+		descriptor.FieldDescriptorProto_TYPE_MESSAGE:
+		field.Type = "RECORD"
+
+	default:
+		return nil, fmt.Errorf("unrecognized field type: %s", desc.GetType().String())
+	}
+
+	switch desc.GetLabel() {
+	case descriptor.FieldDescriptorProto_LABEL_OPTIONAL:
+		field.Mode = "NULLABLE"
+
+	case descriptor.FieldDescriptorProto_LABEL_REQUIRED:
+		field.Mode = "REQUIRED"
+
+	case descriptor.FieldDescriptorProto_LABEL_REPEATED:
+		field.Mode = "REPEATED"
+
+	default:
+		return nil, fmt.Errorf("unrecognized field label: %s", desc.GetLabel().String())
+	}
+
+	if field.Type != "RECORD" {
+		return field, nil
+	}
+
+	recordType, ok := curPkg.lookupType(desc.GetTypeName())
+	if !ok {
+		return nil, fmt.Errorf("no such message type named %s", desc.GetTypeName())
+	}
+	var err error
+	field.Fields, err = convertMessageType(curPkg, recordType)
+	if err != nil {
+		return nil, err
+	}
+
+	return field, nil
+}
+
+func convertMessageType(curPkg *ProtoPackage, msg *descriptor.DescriptorProto) (schema []*Field, err error) {
+	if glog.V(4) {
+		glog.Info("Converting message: ", proto.MarshalTextString(msg))
+	}
+	for _, fieldDesc := range msg.GetField() {
+		field, err := convertField(curPkg, fieldDesc, msg)
+		if err != nil {
+			glog.Errorf("Failed to convert field %s in %s: %v", fieldDesc.GetName(), msg.GetName(), err)
+			return nil, err
+		}
+		schema = append(schema, field)
+	}
+	return
+}
+
+func convertFile(file *descriptor.FileDescriptorProto) ([]*plugin.CodeGeneratorResponse_File, error) {
+	name := path.Base(file.GetName())
+	pkg, ok := globalPkg.relativelyLookupPackage(file.GetPackage())
+	if !ok {
+		return nil, fmt.Errorf("no such package found: %s", file.GetPackage())
+	}
+
+	response := []*plugin.CodeGeneratorResponse_File{}
+	for _, msg := range file.GetMessageType() {
+		options := msg.GetOptions()
+		if options == nil {
+			continue
+		}
+		if !proto.HasExtension(options, E_TableName) {
+			continue
+		}
+		optionValue, err := proto.GetExtension(options, E_TableName)
+		if err != nil {
+			return nil, err
+		}
+		tableName := *optionValue.(*string)
+		if len(tableName) == 0 {
+			return nil, fmt.Errorf("table name of %s cannot be empty", msg.GetName())
+		}
+
+		glog.V(2).Info("Generating schema for a message type ", msg.GetName())
+		schema, err := convertMessageType(pkg, msg)
+		if err != nil {
+			glog.Errorf("Failed to convert %s: %v", name, err)
+			return nil, err
+		}
+
+		jsonSchema, err := json.Marshal(schema)
+		if err != nil {
+			glog.Error("Failed to encode schema", err)
+			return nil, err
+		}
+
+		resFile := &plugin.CodeGeneratorResponse_File{
+			Name:    proto.String(fmt.Sprintf("%s/%s.schema", strings.Replace(file.GetPackage(), ".", "/", -1), tableName)),
+			Content: proto.String(string(jsonSchema)),
+		}
+		response = append(response, resFile)
+	}
+
+	return response, nil
+}
+
+func convert(req *plugin.CodeGeneratorRequest) (*plugin.CodeGeneratorResponse, error) {
+	generateTargets := make(map[string]bool)
+	for _, file := range req.GetFileToGenerate() {
+		generateTargets[file] = true
+	}
+
+	res := &plugin.CodeGeneratorResponse{}
+	for _, file := range req.GetProtoFile() {
+		for _, msg := range file.GetMessageType() {
+			glog.V(1).Infof("Loading a message type %s from package %s", msg.GetName(), file.GetPackage())
+			registerType(file.Package, msg)
+		}
+	}
+	for _, file := range req.GetProtoFile() {
+		if _, ok := generateTargets[file.GetName()]; ok {
+			glog.V(1).Info("Converting ", file.GetName())
+			converted, err := convertFile(file)
+			if err != nil {
+				res.Error = proto.String(fmt.Sprintf("Failed to convert %s: %v", file.GetName(), err))
+				return res, err
+			}
+			res.File = append(res.File, converted...)
+		}
+	}
+	return res, nil
+}
+
+func convertFrom(rd io.Reader) (*plugin.CodeGeneratorResponse, error) {
+	glog.V(1).Info("Reading code generation request")
+	input, err := ioutil.ReadAll(rd)
+	if err != nil {
+		glog.Error("Failed to read request:", err)
+		return nil, err
+	}
+	req := &plugin.CodeGeneratorRequest{}
+	err = proto.Unmarshal(input, req)
+	if err != nil {
+		glog.Error("Can't unmarshal input:", err)
+		return nil, err
+	}
+
+	glog.V(1).Info("Converting input")
+	return convert(req)
+}
+
+func main() {
+	flag.Parse()
+	ok := true
+	glog.Info("Processing code generator request")
+	res, err := convertFrom(os.Stdin)
+	if err != nil {
+		ok = false
+		if res == nil {
+			message := fmt.Sprintf("Failed to read input: %v", err)
+			res = &plugin.CodeGeneratorResponse{
+				Error: &message,
+			}
+		}
+	}
+
+	glog.Info("Serializing code generator response")
+	data, err := proto.Marshal(res)
+	if err != nil {
+		glog.Fatal("Cannot marshal response", err)
+	}
+	_, err = os.Stdout.Write(data)
+	if err != nil {
+		glog.Fatal("Failed to write response", err)
+	}
+
+	if ok {
+		glog.Info("Succeeded to process code generator request")
+	} else {
+		glog.Info("Failed to process code generator but successfully sent the error to protoc")
+		os.Exit(1)
+	}
+}
