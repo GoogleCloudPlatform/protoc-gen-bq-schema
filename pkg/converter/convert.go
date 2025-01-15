f98a6a2b242bf340f1a46c053bc3b2f5551fbc47
Stephen O'Kennedy <stephen.okennedy@cellpointmobile.com>
2025-01-15 16:09:04 +0100

    Enums are now treated as type STRING in BigQuery
    * Enums are now represented as INTEGERS in BigQuery schemas.
    * Updated pluging_test to reflect the change.

--- pkg/converter/convert.go
+++ pkg/converter/convert.go
@@ -47,7 +47,7 @@ var (
 
 		descriptor.FieldDescriptorProto_TYPE_STRING: "STRING",
 		descriptor.FieldDescriptorProto_TYPE_BYTES:  "BYTES",
-		descriptor.FieldDescriptorProto_TYPE_ENUM:   "STRING",
+		descriptor.FieldDescriptorProto_TYPE_ENUM:   "INTEGER",
 
 		descriptor.FieldDescriptorProto_TYPE_BOOL: "BOOLEAN",
 
